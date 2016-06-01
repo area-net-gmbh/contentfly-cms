1a959b845b4010e16f0d04f4094aca4b2ffc8536
areanet <apps@area-net.de>
2016-06-01 18:21:54 +0200

    Deployment

--- deploy/webhook.php
+++ deploy/webhook.php
@@ -4,20 +4,20 @@ if($payload = file_get_contents('php://input')) {
     try {
         $payload = json_decode($payload);
     } catch(Exception $ex) {
-        echo $ex;
+        file_put_contents('log.txt', $ex);
         exit(0);
     }
     // put the branch you want here
     if($payload->ref != "refs/heads/master") {
-        echo "wrong head";
+        file_put_contents('log.txt', "failed request");
         exit(0);
     }
     //put the branch you want here, as well as the directory your site is in
     $result = `cd /html/pim && git pull origin master`;
-
+    file_put_contents('log.txt', "OK");
     echo $result;
 } else {
-    echo "failed request";
+    file_put_contents('log.txt', "failed request");
 }
 
 ?>
