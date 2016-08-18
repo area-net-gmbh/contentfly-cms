f46b7438a2ff60ffdc96ac58b8e8f53cd09872e0
areanet <apps@area-net.de>
2016-08-18 13:29:28 +0200

    Script for Updating Redmine-Server-Wiki

--- redmine-server/update-redmine.php
+++ redmine-server/update-redmine.php
@@ -2,15 +2,16 @@
 $versions = file_get_contents('http://www.das-app-cms.de/download/versions.php');
 $versions = json_decode($versions);
 
+$pdo = new PDO('mysql:host=localhost;dbname=redmine', 'root', 'Heex9ahn');
+
 $data = "h1. Downloads APP-CMS\n\n";
 foreach($versions as $version){
     $data .= "* Version version:".$version->version." - \"Download\":".$version->url."\n";
 
-    $statement = $pdo->prepare("UPDATE version SET status = 'closed' WHERE project = 93 AND name = ?");
+    $statement = $pdo->prepare("UPDATE versions SET status = 'closed' WHERE project_id = 93 AND name = ?");
     $statement->execute(array($version->version));
 }
 
-$pdo = new PDO('mysql:host=localhost;dbname=redmine', 'root', 'Heex9ahn');
 
 $statement = $pdo->prepare("UPDATE wiki_contents SET text = ? WHERE id = 81");
 $statement->execute(array($data));
