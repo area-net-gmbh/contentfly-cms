09638e03504762a2b2b15f5d2697abc5d130b96a
areanet <apps@area-net.de>
2017-08-02 17:40:45 +0200

    Remove XSRF-TOKEN on Logout

--- appcms/areanet/PIM-UI/default/assets/scripts/controllers/logout.controller.js
+++ appcms/areanet/PIM-UI/default/assets/scripts/controllers/logout.controller.js
@@ -16,7 +16,7 @@
         localStorageService.remove('savedFilter');
         localStorageService.remove('uiblocks');
         localStorageService.clearAll();
-        $cookies.remove('token');
+        $cookies.remove('XSRF-TOKEN');
         $rootScope.userLoggedIn = false;
         $rootScope.permissions  = null;
         $location.path('/login');
