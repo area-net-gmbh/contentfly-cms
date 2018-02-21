065b8b76d5377a1cf0fc46a6e45250729d992f68
Florian Schmid <fs@area-net.de>
2018-02-21 16:37:12 +0100

    #68 Multi-Checkbox mit pflegbaren Optionswerten für Many2Many

--- appcms/areanet/PIM/Classes/Types/VirtualjoinType.php
+++ appcms/areanet/PIM/Classes/Types/VirtualjoinType.php
@@ -38,8 +38,8 @@ class VirtualjoinType extends Type
     }
 
 
-    public function processSchema($key, $defaultValue, $propertyAnnotations){
-        $schema             = parent::processSchema($key, $defaultValue, $propertyAnnotations);
+    public function processSchema($key, $defaultValue, $propertyAnnotations, $entityName){
+        $schema             = parent::processSchema($key, $defaultValue, $propertyAnnotations, $entityName);
         $schema['multipe']  = true;
         $schema['dbtype']   = 'string';
         $schema['sortable'] = false;
---
+++ appcms/areanet/PIM/Entity/Option.php
@@ -0,0 +1,85 @@
+<?php
+namespace Areanet\PIM\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Areanet\PIM\Classes\Annotations as PIM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="pim_option")
+ * @PIM\Config(label="Option", labelProperty="value")
+ */
+class Option extends Base
+{
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=false)
+     * @PIM\Config(label="value", showInList=20)
+     */
+    protected $value;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Areanet\PIM\Entity\OptionGroup")
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     */
+    protected $group;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+    /**
+     * @param mixed $group
+     */
+    public function setGroup($group)
+    {
+        $this->group = $group;
+    }
+
+}
+
+
+
